d9cc1532658d9b213117542bdc4b781382a2b41b
Konstantin Yovchev <33600562+zinobg@users.noreply.github.com>
2018-02-15 22:04:31 +0100

    version 1.0.1
    Some code optimization for speed improvement

--- default.py
+++ default.py
@@ -1,67 +1,72 @@
-import urllib2
-import urllib
-import re
-import xbmcplugin
-import xbmcgui
-import xbmcaddon
-import os
+import re,os
+import urllib2,urllib
+import xbmcplugin,xbmcgui,xbmcaddon
 
-from sqlite3 import dbapi2 as sqlite
-try:
-	DB = os.path.join(xbmc.translatePath("special://userdata/Database"), 'Textures13.db')
-	db = sqlite.connect(DB)
-	db.execute('Delete FROM texture WHERE url LIKE "%seirsanduk%"')
-	db.commit()
-	db.close()
-except:
-	pass
+BASE='http://www.seirsanduk.com/'
 
-BASE = "http://www.seirsanduk.com/"
+def LIST_CHANNELS():
+    req=urllib2.Request(BASE)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    response=urllib2.urlopen(req)
+    source=response.read()
+    response.close()
+    match=re.compile('<li><a href="(.+?)"><img src="(.+?)".*>(.+?)</a></li>').findall(source)
+    for url,thumbnail,name in match:
+        thumbnail=BASE+thumbnail
+        addDir(name,url,1,thumbnail)
 
-def CATEGORIES():
-	req = urllib2.Request(BASE)
-	req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-	response = urllib2.urlopen(req)
-	source=response.read()
-	response.close()
-	match=re.compile('<li><a href="(.+?)"><img src="(.+?)".*>(.+?)</a></li>').findall(source)
-	for url,thumbnail,name in match:
-		url = BASE + url
-		thumbnail = BASE + thumbnail
-		req=urllib2.Request(url)
-		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-		response=urllib2.urlopen(req)
-		source=response.read()
-		response.close()
-		match=re.compile('file:"(.+?)"').findall(source)
-		for url in match:
-			addLink(name,url,thumbnail)	
+def INDEX_CHANNELS(name,url):
+    url = BASE + url
+    req=urllib2.Request(url)
+    #req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0')
+    response=urllib2.urlopen(req)
+    source=response.read()
+    response.close()
+    match=re.compile('file:"(.+?)"').findall(source)
+    for url in match:
+        addLink(name,url,'')	
 		
 def get_params():
-        param=[]
-        paramstring=sys.argv[2]
-        if len(paramstring)>=2:
-                params=sys.argv[2]
-                cleanedparams=params.replace('?','')
-                if (params[len(params)-1]=='/'):
-                        params=params[0:len(params)-2]
-                pairsofparams=cleanedparams.split('&')
-                param={}
-                for i in range(len(pairsofparams)):
-                        splitparams={}
-                        splitparams=pairsofparams[i].split('=')
-                        if (len(splitparams))==2:
-                                param[splitparams[0]]=splitparams[1]
-
-        return param
+    param=[]
+    paramstring=sys.argv[2]
+    if len(paramstring)>=2:
+        params=sys.argv[2]
+        cleanedparams=params.replace('?','')
+        if (params[len(params)-1]=='/'):
+            params=params[0:len(params)-2]
+        pairsofparams=cleanedparams.split('&')
+        param={}
+        for i in range(len(pairsofparams)):
+            splitparams={}
+            splitparams=pairsofparams[i].split('=')
+            if (len(splitparams))==2:
+                 param[splitparams[0]]=splitparams[1]
+    return param
 
 def addLink(name,url,iconimage):
-        ok=True
-        liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name } )
-        liz.setProperty('IsPlayable','true')
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
-        return ok
+    ok=True
+    liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
+    liz.setInfo( type="Video", infoLabels={ "Title": name } )
+    liz.setProperty('IsPlayable','true')
+    ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+    return ok
+
+def playLink(name,url):
+    ok=True
+    liz=xbmcgui.ListItem(name,iconImage="DefaultVideo.png",thumbnailImage='')
+    liz.setInfo(type="Video", infoLabels={ "Title": name })
+    liz.setProperty('IsPlayable','true')
+    ok=xbmc.Player().play(url,liz)
+    return ok
+
+def addDir(name,url,mode,iconimage):
+    u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+    ok=True
+    liz=xbmcgui.ListItem(name,iconImage="DefaultFolder.png",thumbnailImage=iconimage)
+    liz.setInfo(type="Video", infoLabels={ "Title": name })
+    ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+    return ok
 
 params=get_params()
 url=None
@@ -81,12 +86,14 @@ try:
 except:
         pass
 
-print "Mode: "+str(mode)
-print "URL: "+str(url)
-print "Name: "+str(name)
+xbmc.log("Mode: "+str(mode))
+xbmc.log("URL: "+str(url))
+xbmc.log("Name: "+str(name))
 
 if mode==None or url==None or len(url)<1:
-        print ""
-        CATEGORIES()
+        LIST_CHANNELS()
+
+elif mode==1:
+    INDEX_CHANNELS(name,url)
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
