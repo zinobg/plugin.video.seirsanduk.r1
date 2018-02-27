760869428586173599387a39a4013e60dd477ea3
Konstantin Yovchev <33600562+zinobg@users.noreply.github.com>
2018-02-27 18:05:22 +0100

    Version 2.2
    - Alt stream link added
    - if no username entered, tries to play the channel anyway
    - if username is entered, tries to login

--- default.py
+++ default.py
@@ -8,34 +8,54 @@ username=xbmcaddon.Addon().getSetting('username')
 password=xbmcaddon.Addon().getSetting('password')
 
 BASE='http://www.seirsanduk.com/'
+header_string='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0'
+
+def openUrl(url):
+    req=urllib2.Request(url)
+    req.add_header('User-Agent',header_string)
+    response=urllib2.urlopen(req)
+    source=response.read()
+    response.close()
+    return source
 
 # checks if user is logging from Bulgaria 
-req=urllib2.Request(BASE)
-req.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0')
-response=urllib2.urlopen(req)
-source=response.read()
-response.close()
-if 'loginbg' in source:
+source_main=openUrl(BASE)
+if 'loginbg' in source_main:
     login='loginbg/'
 else:
     login='login/'
-BASE=BASE+login
+BASE_LOG=BASE+login
 
 def LIST_CHANNELS():
-    url=BASE+'index.php'
-    source=weblogin.doLogin('',username,password,url)
+    url=BASE_LOG+'index.php'
+    if username=='':
+        source=source_main
+    else:
+        source=weblogin.doLogin('',username,password,url)
     match=re.compile('<li><a href="(.+?)"><img src="(.+?)".*>(.+?)<\/a><\/li>').findall(source)
     for url_chann,thumbnail,name in match:
-        url_chann=url+url_chann
         thumbnail=BASE+thumbnail
         addDir(name,url_chann,1,thumbnail)
 
 def INDEX_CHANNELS(name,url):
-    channel_source=weblogin.doLogin('',username,password,url)
-    match=re.compile('file:"(.+?)"').findall(channel_source)
-    name='PLAY: '+name
-    for url in match:
-        addLink(name,url,'')	
+    if username=='':
+        url=BASE+url
+        channel_source=openUrl(url)
+    else:
+        url=BASE_LOG+'index.php'+url
+        channel_source=weblogin.doLogin('',username,password,url)
+    match_link_01=re.compile('file:"(.+?)"').findall(channel_source)
+    match_link_02=re.compile('<a id="link" href=(.+?)>').findall(channel_source)
+    name_01='PLAY: '+name
+    for url_01 in match_link_01:
+        addLink(name_01,url_01,'')
+    for url_02 in match_link_02:
+        url_02=BASE+url_02
+        source_alt=openUrl(url_02)
+        match_link_02=re.compile('file:"(.+?)"').findall(source_alt)
+        for url_02 in match_link_02:
+            name_02='PLAY ALT: '+name
+            addLink(name_02,url_02,'')
 		
 def get_params():
     param=[]
