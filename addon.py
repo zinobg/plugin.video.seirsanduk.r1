5bf8fc964cd18bf7e70c5c4e38aff456782c9319
Konstantin Yovchev <zinobg@gmail.com>
2020-04-03 23:13:01 +0200

    version 4.1.3

--- addon.py
+++ addon.py
@@ -1,21 +1,33 @@
 # -*- coding: utf-8 -*-
-from urllib2 import Request,urlopen
+import urllib2
 from re import compile as Compile
 from xbmc import log
 from xbmcgui import Dialog
 from xbmcswift2 import Plugin
 
-BASE='http://www.seirsanduk.com/'
+BASE=['http://www.seirsanduk.com/','http://www.seirsanduk.online']
+
+header='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0'
 
 plugin=Plugin()
+
+opener=urllib2.build_opener(urllib2.HTTPHandler,urllib2.HTTPRedirectHandler())
+urllib2.install_opener(opener)
 
 @plugin.route('/')
 def index():
     log('path: [/]')
     items=[]
-    source=openUrl(BASE)
-    match=Compile('<a href="(.+?)"><img src="(.+?)".*>(.+?)<\/a').findall(source)
-    items=[{'label':name,'thumbnail':BASE+thumbnail,'path':plugin.url_for('index_source',url='http:'+url,name=name,icon=BASE+thumbnail)} for url,thumbnail,name in match]
+    for B in BASE:
+        try:
+            source=openUrl(B)
+        except:
+            source=None
+        match=Compile('<a href="(.+?)"><img src="(.+?)".*>(.+?)<\/a').findall(source)
+        if not match:
+            continue
+        break
+    items=[{'label':name,'thumbnail':B+thumbnail,'path':plugin.url_for('index_source',url='http:'+url,name=name,icon=B+thumbnail)} for url,thumbnail,name in match]
     return plugin.finish(items)
  
 @plugin.route('/stream/<url>/<name>/<icon>/')
@@ -27,11 +39,11 @@ def index_source(url,name,icon):
     plugin.play_video(item)
     Dialog().notification(name,'',icon,8000,sound=False)
     return plugin.finish(None,succeeded=False)
-    
+   
 def openUrl(url):
-    req=Request(url)
-    req.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0')
-    response=urlopen(req)
+    req=urllib2.Request(url)
+    req.add_header('User-Agent',header)
+    response=urllib2.urlopen(req)
     source=response.read()
     response.close()
     return source
